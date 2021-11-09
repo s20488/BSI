90129feaa34acfc41b520b7c9a6b1afa90dbb604
Anastasiia Ponkratova <56721899+s20488@users.noreply.github.com>
2021-11-09 21:21:49 +0200

    Delete comparing_probability_of_success_of_events.h

--- comparing_probability_of_success_of_events.h
+++
@@ -1,7 +0,0 @@
-#ifndef BSI_RELIABILITY_CALCULATOR_COMPARING_PROBABILITY_OF_SUCCESS_OF_EVENTS_H
-#define BSI_RELIABILITY_CALCULATOR_COMPARING_PROBABILITY_OF_SUCCESS_OF_EVENTS_H
-long long gcd(long long a, long long b);
-void decimalToFraction(double input);
-double number_of_combinations(int n, int k);
-void probability_of_success();
-#endif
