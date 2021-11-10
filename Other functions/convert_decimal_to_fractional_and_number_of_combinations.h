d963871bb62e1379a8510853eb8a36855f7c2b2a
Anastasiia Ponkratova <56721899+s20488@users.noreply.github.com>
2021-11-10 19:14:47 +0200

    Delete Other functions directory

--- Other functions/convert_decimal_to_fractional_and_number_of_combinations.h
+++
@@ -1,6 +0,0 @@
-#ifndef BSI_RELIABILITY_CALCULATOR_CONVERT_DECIMAL_TO_FRACTIONAL_AND_NUMBER_OF_COMBINATIONS_H
-#define BSI_RELIABILITY_CALCULATOR_CONVERT_DECIMAL_TO_FRACTIONAL_AND_NUMBER_OF_COMBINATIONS_H
-long long gcd(long long a, long long b);
-void decimalToFraction(double input);
-double number_of_combinations(int n, int k);
-#endif
